ca7d7ecd7f647204e9e9e403e519e41c7801bec6
kinson4wu <kingson_wu@163.com>
2014-09-13 15:35:17 +0800

    first commit

---
+++ KingsonTemplate/src/resource.js
@@ -0,0 +1,24 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_CloseNormal = "CloseNormal.png";
+var s_CloseSelected = "CloseSelected.png";
+// 将资源写在这里，程序会预加载这些资源
+var s_Plane = "plane.png";
+
+
+var g_resources = [
+    //image
+    s_HelloWorld,
+    s_CloseNormal,
+    s_CloseSelected,
+    s_Plane
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
